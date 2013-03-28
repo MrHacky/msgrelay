4e152c36b18e990d6e8e6024f088b95428712332
Simon Sasburg <simon.sasburg@gmail.com>
2013-03-28 01:10:21 +0100

    Test for github-pusher

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello 2 from GitHub pusher!');
+  res.end('Hello 3 from GitHub pusher!');
 }).listen(process.env.VMC_APP_PORT || 1337, null);

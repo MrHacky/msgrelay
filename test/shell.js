82c1a64b5f340933d79acdf1384a019ef1542871
Simon Sasburg <simon.sasburg@gmail.com>
2013-03-30 19:12:09 +0100

    fx

--- test/shell.js
+++ test/shell.js
@@ -1,22 +1,19 @@
 module.exports = function(req, res) {
-  res.send('Hello World 1');
+	var spawn = require('child_process').spawn;
+	var ls    = spawn('ls', ['-lh', '/usr']);
 
-var spawn = require('child_process').spawn,
-    ls    = spawn('ls', ['-lh', '/usr']);
+	var output = "";
 
-var output = "";
+	ls.stdout.on('data', function (data) {
+	output += ('stdout: ' + data) + "\n";
+	});
 
-ls.stdout.on('data', function (data) {
-  output += ('stdout: ' + data) + "\n";
-});
+	ls.stderr.on('data', function (data) {
+	output += ('stderr: ' + data) + "\n"
+	});
 
-ls.stderr.on('data', function (data) {
-  output += ('stderr: ' + data) + "\n"
-});
-
-ls.on('close', function (code) {
-  output += 'child process exited with code ' + code;
-  res.send(output);
-});
-
+	ls.on('close', function (code) {
+		output += 'child process exited with code ' + code;
+		res.send(output);
+	});
 }
